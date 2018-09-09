253b749a1c5d9b8d4d115cf223493c19ddb75062
dhivyanand <dhivyananthan98@gmail.com>
2018-09-09 21:01:31 +0530

    Cloud Firestore Initialization

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
- exports.sw = functions.https.onRequest((request, response) => {
-  response.send("Welcome to Smart Watch Data Sync.");
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
+
+ exports.test_add = functions.https.onRequest((request, response) => {
+  
+    var docRef = db.collection('admin').doc('a');
+
+    var setAda = docRef.set({
+        req: "hello world"
+    });
+
+    response.send("Added \"Hello World\" as a data to firestore");
  });
