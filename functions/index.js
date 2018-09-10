76bea2d79352fc03e5653af86e78e594e0e61fdd
dhivyanand <dhivyananthan98@gmail.com>
2018-09-10 18:48:02 +0530

    Migration from Firebase Cloud Firestore to Firebase Realtime Database and Definition of test function

--- functions/index.js
+++ functions/index.js
@@ -13,12 +13,19 @@ var collections = db.collection('admin').doc('smartWatch');
     var password = request.body.password;
     var email = request.body.email;
 
-    var user_reg = collections.collection('User').doc(email).set({
-        password: password,
-        name: uname,
-        otp: ""
-    });
+ });
 
-    
-    response.send("User Created");
+ exports.login = functions.https.onRequest((request, response) => {
+
+    var id = request.body.id;
+    var password = request.body.password;
+ 
+ });
+
+ exports.testFunc = functions.https.onRequest((request, response) => {
+
+
+  response.send('Functions working');
+
+
  });
